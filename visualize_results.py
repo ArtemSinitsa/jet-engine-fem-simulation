11369f17ca16c3cd7dd8b03bc91d1be504c48e0b
ArtemSinitsa <artyomsinitsa@gmail.com>
2025-06-16 16:06:29 +0200

    Update visualize_results.py

--- visualize_results.py
+++ visualize_results.py
@@ -1,1 +1,13 @@
+import pyvista as pv
+
+# Load VTK result
+mesh = pv.read("result.vtk")
+
+# Plot displacement magnitude
+displacement = mesh.point_data['u']
+mesh['Displacement Magnitude'] = (displacement**2).sum(axis=1)**0.5
+
+plotter = pv.Plotter()
+plotter.add_mesh(mesh, scalars='Displacement Magnitude', cmap="viridis")
+plotter.show()
 
