d94402bf3a73051160b02f63c76cadc46814b8e3
ArtemSinitsa <artyomsinitsa@gmail.com>
2025-06-16 16:04:45 +0200

    Update generate_mesh.py

--- generate_mesh.py
+++ generate_mesh.py
@@ -1,1 +1,32 @@
+import gmsh
+import sys
+import os
 
+def generate_jet_engine_mesh():
+    gmsh.initialize()
+    gmsh.option.setNumber("General.Terminal", 1)
+    gmsh.model.add("jet_engine")
+
+    # Create a simple disk + blade block to simulate a rotor hub
+
+    # Rotor disk (cylinder)
+    disk = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, 0.2, 0.5)
+
+    # Simplified blade (box on top of disk)
+    blade = gmsh.model.occ.addBox(0.15, 0, 0.2, 0.1, 0.05, 0.2)
+
+    gmsh.model.occ.fuse([(3, disk)], [(3, blade)], removeObject=True, removeTool=True)
+    gmsh.model.occ.synchronize()
+
+    # Mesh parameters
+    gmsh.option.setNumber("Mesh.CharacteristicLengthMin", 0.02)
+    gmsh.option.setNumber("Mesh.CharacteristicLengthMax", 0.05)
+
+    gmsh.model.mesh.generate(3)
+    gmsh.write("jet_engine_mesh.msh")
+
+    gmsh.finalize()
+
+if __name__ == "__main__":
+    generate_jet_engine_mesh()
+sfepy-convert jet_engine_mesh.msh jet_engine_mesh.mesh
